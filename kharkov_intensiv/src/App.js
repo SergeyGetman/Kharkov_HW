be630a0c850af66dd6f0f4956345c09d06efde98
Sergey <sergeygetman2020@gmail.com>
2021-10-29 14:34:12 +0300

    add component

--- kharkov_intensiv/src/App.js
+++ kharkov_intensiv/src/App.js
@@ -1,12 +1,19 @@
-let person = {
-  name: "Yoda",
-  designation: "Jedi Master ",
+import React, { useState } from "react";
+import "./App.css";
+import TitleShow from "./components/Title";
+
+const focus = (e) => {
+  console.log(e);
+  return <TitleShow />;
 };
+
+// const [state, setState] = useState({});
 
 function App() {
   return (
     <div>
       <h1>Hello My Many project</h1>
+      <button onClick={focus}>click me </button>
     </div>
   );
 }
---
+++ kharkov_intensiv/src/components/Title.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TitleShow() {
+  return <div className="title_header">Welcome on a boart</div>;
+}
